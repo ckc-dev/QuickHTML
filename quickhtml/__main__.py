58f4e851707a9c81d045a0280a348b0919cf49a7
ckc <ckc-dev@pm.me>
2021-08-20 16:27:24 -0300

    Refactor codebase.
    Made multiple small refactorings, such as fixing and removing unnecessary comments and making code more consistent, thus improving readability and maintainability.

--- quickhtml/__main__.py
+++ quickhtml/__main__.py
@@ -1,43 +1,24 @@
-"""
-This file is executed when running the module directly.
-"""
+"""This file is executed when running the module directly."""
 
 import os
 import sys
+
 from quickhtml import convert, convert_file
 
-# Initialize constants.
 ARGS = sys.argv[1:]
 MESSAGES = {
-    "NO_ARGUMENT":
-    """No file or string was provided.
-Use "python -m quickhtml -h" or "python -m quickhtml --help" to print a help message.""",
-
-    "HELP":
-    """To convert Markdown into HTML, use "python -m quickhtml [args]",
-where [args] is a list of arguments, and each argument is either a file or a string.
-
-E.g., "python -m quickhtml FILE.md "# This is a level 1 heading" FILE_2.md".
-
-To export results to a file, use "python -m quickhtml [args] > [out_file]".
-
-To see this message, run "python -m quickhtml -h" or "python -m quickhtml --help".""",
+    "NO_ARGUMENT": "No file or string was provided. Use \"python -m quickhtml -h\" or \"python -m quickhtml --help\" to print a help message.",
+    "HELP": "To convert Markdown into HTML, use \"python -m quickhtml [args]\", where [args] is a list of arguments, and each argument is either a file or a string.\nE.g.: \"python -m quickhtml FILE.md \"# This is a level 1 heading\" FILE_2.md\".\nTo export results to a file, use \"python -m quickhtml [args] > [out_file]\".\nTo see this message, run \"python -m quickhtml -h\" or \"python -m quickhtml --help\".",
 }
 
 
 def main():
-    """
-    Converts Markdown into HTML and prints it to the terminal.
-    """
-
-    # No argument was provided or arguments were empty strings.
+    """Convert Markdown into HTML and print it to the terminal."""
     if not ARGS or all(arg.strip() == "" for arg in ARGS):
         print(MESSAGES["NO_ARGUMENT"])
-    # Display help message.
     elif len(ARGS) == 1 and ARGS[0] in ("--help", "-h"):
         print(MESSAGES["HELP"])
     else:
-        # Convert and print each argument.
         for arg in ARGS:
             print(convert_file(arg) if os.path.isfile(arg) else convert(arg))
 
