398105be06c68e6e411160ae74438b0031ffbbb0
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2021-09-05 06:55:22 -0300

    Update package version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="quickhtml",
-    version="1.0.0",
+    version="2.0.0",
     author="ckc-dev",
     author_email="ckc-dev@pm.me",
     description="A simple Markdown to HTML preprocessor.",
